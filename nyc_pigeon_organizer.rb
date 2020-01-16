7b5bc277e11735ffa9055e24ba76e4c3881864a0
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-16 23:07:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,8 +20,6 @@ def nyc_pigeon_organizer(data)
   name_pigeon = data[:gender][:male] + data[:gender][:female]
   pigeon_list =  Hash.new
   keys_collect = Hash[data.collect{|key, value| [key, 0]}]
-  pp keys_collect
-
   i = 0
       while i < name_pigeon.length do
         pigeon_list[name_pigeon[i]] = keys_collect
