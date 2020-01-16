e83913f1dd6795a880224df15173e3c85be65608
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-16 22:58:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+require "pp"
+
+
+def search_in_list(name_pigeon, category_pigeon)
+  new_array = []
+
+  category_pigeon.each do |key, value|
+      if value.include?(name_pigeon) == true
+              s = "#{key}"
+              new_array.push(s)
+            else
+              nil
+            end
+  end
+  new_array
+end
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  name_pigeon = data[:gender][:male] + data[:gender][:female]
+  pigeon_list =  Hash.new
+  keys_collect = Hash[data.collect{|key, value| [key, 0]}]
+
+  i = 0
+      while i < name_pigeon.length do
+        pigeon_list[name_pigeon[i]] = keys_collect
+          if pigeon_list[name_pigeon[i]].has_value?(0) == true
+            pigeon_list[name_pigeon[i]][:color] = search_in_list(name_pigeon[i], data[:color])
+          elsif pigeon_list[name_pigeon[i]].has_value?(0) == false
+            i += 1
+          end
+          pp pigeon_list
+      end
 end
